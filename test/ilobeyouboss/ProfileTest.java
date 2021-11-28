455800f6165e22b3986268c1ee104ffc859817ca
JangHyeonJun2 <epfvkdlxj@naver.com>
2021-11-28 17:10:46 +0900

    2장 [2.5 @Before 메서드로 테스트 초기화]

--- test/ilobeyouboss/ProfileTest.java
+++ test/ilobeyouboss/ProfileTest.java
@@ -1,17 +1,26 @@
 package ilobeyouboss;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ProfileTest {
+    private Profile profile;
+    private BooleanQuestion question;
+    private Criteria criteria;
+
+    @BeforeEach
+    public void create() {
+        profile = new Profile("Bull Hockey, Inc.");
+        question = new BooleanQuestion(1, "Got Bonuses?");
+        criteria = new Criteria();
+    }
     @Test
     public void matchAnswersFalseWhenMustMatchCriteriaNotMet() {
-        Profile profile = new Profile("Bull Hockey, Inc.");
-        Question question = new BooleanQuestion(1, "Got bonuses?");
         Answer profileAnswer = new Answer(question, Bool.FALSE);
         profile.add(profileAnswer);
-        Criteria criteria = new Criteria();
         Answer criteriaAnswer = new Answer(question, Bool.TRUE);
         Criterion criterion = new Criterion(criteriaAnswer, Weight.MustMatch);
 
@@ -23,11 +32,8 @@ class ProfileTest {
 
     @Test
     public void matchAnswersTrueForAnyDontCareCriterial() {
-        Profile profile = new Profile("Bull Hockey, Inc");
-        Question question = new BooleanQuestion(1, "Got milk");
         Answer profileAnswer = new Answer(question, Bool.FALSE);
         profile.add(profileAnswer);
-        Criteria criteria = new Criteria();
         Answer criteriaAnswer = new Answer(question, Bool.TRUE);
         Criterion criterion = new Criterion(criteriaAnswer, Weight.DontCare);
         criteria.add(criterion);
